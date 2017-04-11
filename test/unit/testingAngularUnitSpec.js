82311c4453bf0505eb4203e201ae03e0d9d01100
Andela Developer <andeladeveloper@Eyo-MacBook-Pro-2.local>
2017-04-12 00:17:51 +0100

    adds test folder

--- test/unit/testingAngularUnitSpec.js
+++ test/unit/testingAngularUnitSpec.js
@@ -1,15 +1,16 @@
-describe('Testing  AngularJS Test Suite', function() {
-    describe('Testing header Controller', function() {
-        it('should test scope object', function() {
-            module('mean.system');
-            var scope = {};
-            var ctrl;
-            inject( function($controller) {
-                ctrl = $controller('HeaderController', { $scope: scope, Global: { test: 'test' } });
-            });
-            expect(scope.menu.length).toBe(2);
-            expect(typeof scope.global).toBe("object");
-            expect(scope.global.test).toBe('test');
-        });
+describe('Testing  AngularJS Test Suite', function () {
+  describe('Testing header Controller', function () {
+    it('should test scope object', function () {
+      module('mean.system');
+      var scope = {};
+      var ctrl;
+      inject(function ($controller) {
+        ctrl = $controller('HeaderController', { $scope: scope, Global: { test: 'test' } });
+      });
+      expect(scope.menu.length).toBe(2);
+      expect(typeof scope.global).toBe("object");
+      expect(scope.global.test).toBe('test');
     });
+  });
 });
+
