b21aeb2eff9fb2e085fbbea56234e3aa56e3a3ca
Eyo <andeladeveloper@Eyo-MacBook-Pro-2.local>
2017-04-12 16:41:05 +0100

    fix errors raised by eslint

--- test/unit/testingAngularUnitSpec.js
+++ test/unit/testingAngularUnitSpec.js
@@ -4,16 +4,12 @@ describe('Testing  AngularJS Test Suite', function() {
             module('mean.system');
             var scope = {};
             var ctrl;
-
-            inject(function($controller) {
-                ctrl = $controller('HeaderController', {$scope:scope, Global:{test:'test'}});
+            inject( function($controller) {
+                ctrl = $controller('HeaderController', { $scope: scope, Global: { test: 'test' } });
             });
             expect(scope.menu.length).toBe(2);
             expect(typeof scope.global).toBe("object");
             expect(scope.global.test).toBe('test');
-
         });
-
     });
-
-})
+});
