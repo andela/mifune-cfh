81c264574f2eda1bdca32b6ae076d88514083259
Folusho Oladipo <folusho.oladipo@andela.com>
2017-04-25 12:44:55 +0100

    upgrade the version of angular and some its modules

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,4 @@
-angular.module('mean', ['ngCookies', 'ngResource', 'ui.bootstrap', 'ui.route', 'mean.system', 'mean.directives'])
+angular.module('mean', ['ngCookies', 'ngResource', 'ui.bootstrap', 'ngRoute', 'mean.system', 'mean.directives'])
 // TODO: REPLACE UI.ROUTE WITH NGROUTE.
   .config(['$routeProvider',
       function($routeProvider) {
