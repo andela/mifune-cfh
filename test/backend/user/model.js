49c63c501f6e0909924a8e572afa2f4e6fa38f88
andela-onnenanya <obinna.nnenanya@andela.com>
2017-04-25 11:13:55 +0100

    chore(travis):update to es6

--- test/backend/user/model.js
+++ test/backend/user/model.js
@@ -3,7 +3,7 @@
  */
 /* eslint-disable */
 
-var should = require('should'),
+const should = require('should'),
     app = require('../../../server'),
     mongoose = require('mongoose'),
     User = mongoose.model('User');
@@ -13,36 +13,34 @@ var user;
 
 //The tests
 describe('<Unit Test>', function() {
-    describe('Model User:', function() {
-        before(function(done) {
-            user = new User({
-                name: 'Full name',
-                email: 'test@test.com',
-                username: 'user',
-                password: 'password'
-            });
+  describe('Model User:', function() {
+    before(function(done) {
+      user = new User({
+        name: 'Full name',
+        email: 'test@test.com',
+        username: 'user',
+        password: 'password'
+      });
 
-            done();
-        });
-
-        describe('Method Save', function() {
-            it('should be able to save whithout problems', function(done) {
-                user.save(function(err) {
-                    console.log(err);
-                    should.not.exist(err);
-                    done();
-                });
-            });
-
-            it('should be able to show an error when try to save witout name', function(done) {
-                user.name = '';
-                user.save(function(err) {
-                    should.exist(err);
-                    done();
-                });
-            });
-        });
-
-       
+      done();
     });
+
+    describe('Method Save', function() {
+      it('should be able to save whithout problems', function(done) {
+        user.save(function(err) {
+          console.log(err);
+          should.not.exist(err);
+          done();
+        });
+      });
+
+      it('should be able to show an error when try to save witout name', function(done) {
+        user.name = '';
+        user.save(function(err) {
+          should.exist(err);
+          done();
+        });
+      });
+    });
+  });
 });
