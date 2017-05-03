098829f5981b7e1a25a3ce1fcf87b57638d6c737
andela-onnenanya <obinna.nnenanya@andela.com>
2017-05-03 16:45:54 +0100

    feature(game):add start button and controller

--- public/js/init.js
+++ public/js/init.js
@@ -1,3 +1,4 @@
+/*eslint-disable */
 // window.bootstrap = function() {
 //     angular.bootstrap(document, ['mean']);
 // };
@@ -13,3 +14,23 @@ if (window.location.hash == "#_=_") window.location.hash = "#!";
 //     //Then init the app
 //     window.init();
 // });
+$("body").on("click","#start_game_action", () => {
+  swal({
+  title: "Are you sure?",
+  text: "You want to start the game now?",
+  type: "success",
+  showCancelButton: true,
+  confirmButtonColor: "#DD6B55",
+  confirmButtonText: "Yes Am Ready",
+  cancelButtonText: "No, Not Now",
+  closeOnConfirm: false,
+  closeOnCancel: false
+},
+function(isConfirm){
+  if (isConfirm) {
+    swal("Weldone", "Click okay to start", "success");
+  } else {
+    swal("Cancelled", "You are off! Shitty you!!!", "error");
+  }
+});
+});
